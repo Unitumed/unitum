fab3fb9781e0a4dc30bac25ff4e5f6b0310df7e5
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-08-16 19:38:46 +0000

    Fix: Remove dist

--- dist/utils/use.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.use = void 0;
-const use = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
-exports.use = use;
